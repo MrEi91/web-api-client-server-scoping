635580e48a4ef7517577547bb5990533f7d9ab9f
Eri Irawan <email": null>
2017-03-23 22:38:29 +0700

    add list foods

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,10 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/index', (req, res) => {
-  res.send('SUCCSESS')
+  let meal = ['Fried chiken', 'pecel lele', 'Mie Ayam', 'Martabak', 'Roti Bakar']
+  res.json({
+    meal: meal
+  })
 })
 
 module.exports = router
